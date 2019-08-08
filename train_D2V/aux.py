c3c4c89a7ebf001948d911861c211db723e50b73
agilll <alberto.gil.solla@gmail.com>
2019-08-08 18:52:17 +0200

    Add files via upload

---
+++ train_D2V/aux.py
@@ -0,0 +1,36 @@
+
+
+# folders and filenames involved in corpus construction
+MODELS_FOLDER = "../models"
+KORPUS_FOLDER = "../KORPUS"
+
+LEE_D2V_MODEL = MODELS_FOLDER + "/model_d2v_lee.model"
+
+OWN_D2V_MODEL = MODELS_FOLDER + "/model_d2v_own.model"
+
+TRAINING_TEXTS_FOLDER = KORPUS_FOLDER + '/original_texts'
+
+#DEFAULT_TRAINING_TEXTS = "historical_modify.txt"
+DEFAULT_TRAINING_TEXTS = "originales.s.w"
+
+
+
+
+URL_Stanford = "http://localhost:9000"
+
+# class to manage word identification with the Stanfors tool
+class StanfordBroker:
+	# to init a broker to the Stanford service, that must be running in this host
+	def __init__(self):
+		self.nlpStanfordBroker =  StanfordCoreNLP(URL_Stanford)  
+		
+	# to request identification of words in a sentence
+	def identifyWords (self, sentence):		
+		# pasamos la frase por el stanford para saber qué tipo de palabras componen la frase  (res tiene el formato que se puede ver en la sec 6.1)
+		res = self.nlpStanfordBroker.annotate(sentence, properties={'annotators': 'tokenize,ssplit,pos,lemma', 'outputFormat': 'json'})
+		
+		return res["sentences"][0]["tokens"]
+	
+	
+	
+	
---
+++ train_D2V/buildD2VLeeModel.py
@@ -0,0 +1,117 @@
+import os
+import collections
+import random
+
+# To install: pip install smart_open
+from smart_open import open as smart_open
+
+# to install: pip3 install gensim
+import gensim
+from gensim.models.doc2vec import Doc2Vec
+
+LEE_TRAINING_CORPUS = 'lee_background.cor'
+LEE_TESTING_CORPUS = 'lee.cor'
+
+def buildDoc2VecModel():
+	from aux import LEE_D2V_MODEL as _LEE_D2V_MODEL
+	# Set file names for training and testing data
+	test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
+
+	# Use the Lee Background corpus dataset (set of documents) included in gensim library to train the model
+	training_data = test_data_dir + os.sep + LEE_TRAINING_CORPUS
+
+	# Use the Lee Corpus included gensim to test the model
+	testing_data = test_data_dir + os.sep + LEE_TESTING_CORPUS
+
+
+	# Read and Pre-process Text
+	def readCorpus(fileName, tokens_only=False):
+		# smart_open is used for streaming very large files
+		# Use the latin encoding
+		with smart_open(fileName, encoding="iso-8859-1") as file:
+			for index, line in enumerate(file):
+				# Use the tokens_only to process the training and testing data differently
+				if tokens_only:
+					# Use gensim.utils.simple_preprocess for processing:
+					# tokenize text to individual words
+					# remove punctuations
+					# set to lowercase
+					# remove words less than 2 characters or more than 15 characters
+
+					# Return a generator with processed text
+					yield gensim.utils.simple_preprocess(line)
+				else:
+					# tokens_only = true for training data
+
+					# Returns a generator with tagged training corpus
+					yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [index])
+
+
+	# Convert generators to lists
+	training_corpus = list(readCorpus(training_data))
+	testing_corpus = list(readCorpus(testing_data, tokens_only=True))
+
+
+	# Training the model #
+
+	# Instantiate a Doc2Vec Object
+	# vector_size: vector dimensions
+	# min_count: the minimum number of a word frequency to be included
+	# epochs: Number of iterations over the training corpus
+	model = gensim.models.doc2vec.Doc2Vec(vector_size=40, min_count=2, epochs=40)
+
+	# Build a Vocabulary
+	model.build_vocab(training_corpus)
+
+
+	# Model assessment with the training dataset #
+
+	ranks = []
+	second_ranks = []
+
+	# Go through each document of the training corpus
+	for doc_index in range(len(training_corpus)):
+		# Infer a new vector to training corpus documents
+		inferred_vector = model.infer_vector(training_corpus[doc_index].words)
+
+		self_similarity = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+		rank = [docindex for docindex, sim in self_similarity].index(doc_index)
+		ranks.append(rank)
+		second_ranks.append(self_similarity[1])
+
+
+	# Save model to disk, so we won't need to do the training everytime
+	model.save(_LEE_D2V_MODEL)
+
+	# Count how each document ranks with respect to the training corpus
+	documents_ranks = collections.Counter(ranks)
+
+	print('Document ({}): {}\n'.format(doc_index, ' '.join(training_corpus[doc_index].words)))
+
+	print('Documents similarity')
+
+	self_similarity_length = len(self_similarity)
+
+
+	for label, index in [('Most', 0), ('Second Most', 1), ('Median', self_similarity_length//2), ('Least', self_similarity_length-1)]:
+		print(u'%s %s: %s\n' % (label, self_similarity[index], ' '.join(training_corpus[self_similarity[index][0]].words)))
+
+	# Testing #
+
+	testing_corpus_length = len(testing_corpus)
+	# Generate a random id from the testing corpus
+	doc_index = random.randint(0, testing_corpus_length-1)
+
+	# Infer a vector from the model
+	inferred_vector = model.infer_vector(testing_corpus[doc_index])
+
+	self_similarity = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+	print('Document ({}): {}\n'.format(doc_index, ' '.join(training_corpus[doc_index].words)))
+
+
+	for label, index in [('Most', 0), ('Second Most', 1), ('Median', self_similarity_length//2), ('Least', self_similarity_length-1)]:
+		print(u'%s %s: %s\n' % (label, self_similarity[index], ' '.join(training_corpus[self_similarity[index][0]].words)))
+
+
+# Build a doc2vec model
+buildDoc2VecModel()
