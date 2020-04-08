6410808cb581d9ee67621ba5948e3f4be4c3b881
Alberto <alberto.gil.solla@gmail.com>
2020-04-08 13:56:18 +0200

    cambios

--- module_train/buildD2VOwnModel-t.py
+++ module_train/buildD2VOwnModel-t.py
@@ -1,77 +1,57 @@
-# with 100 epochs, min_count=5, and .t
+# training with 100 epochs, min_count=5, and .t files
+# .t files are preprocessed .txt files
+# a .t file contains a binary structure (pickle saved): a list of lists of words (already lemmas). That is, tokenization has already been done
 
 import glob
 import os
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from nltk.tokenize import word_tokenize
 import pickle
 
+from aux import TRAINING_T_FOLDER as _TRAINING_T_FOLDER, OWN_D2V_MODEL as _OWN_D2V_MODEL
 
-from aux import CORPUS_FOLDER as _CORPUS_FOLDER
-if not os.path.exists(_CORPUS_FOLDER):
-	print(_CORPUS_FOLDER, "not found!")
-	exit()
-	
-from aux import TRAINING_T_FOLDER as _TRAINING_T_FOLDER
 if not os.path.exists(_TRAINING_T_FOLDER):
 	print(_TRAINING_T_FOLDER, "not found!")
 	exit()
 
 
+# variables init
 
-from aux import OWN_D2V_MODEL as _OWN_D2V_MODEL
-model_full_filename = _OWN_D2V_MODEL + "-t.model"
+model_filename = _OWN_D2V_MODEL + "-t.model"
 
-
-
-# vector_size (int, optional) – Dimensionality of the feature vectors
-vector_size = 20
-
-# window (int, optional) – The maximum distance between the current and predicted word within a sentence
-window = 8
-
-# alpha (float, optional) – The initial learning rate
-alpha = 0.025
-
-# min_alpha (float, optional) – Learning rate will linearly drop to min_alpha as training progresses
-min_alpha = 0.00025
+vector_size = 20	# vector_size (int, optional) – Dimensionality of the feature vectors
+window = 8	# window (int, optional) – The maximum distance between the current and predicted word within a sentence
+alpha = 0.025	# alpha (float, optional) – The initial learning rate
+min_alpha = 0.00025	# min_alpha (float, optional) – Learning rate will linearly drop to min_alpha as training progresses
 
 # seed (int, optional) – Seed for the random number generator. Initial vectors for each word are seeded with a hash of the concatenation of word + str(seed)
 # seed = 1
 
-# min_count (int, optional) – Ignores all words with total frequency lower than this
-min_count = 5
-
-# max_vocab_size (int, optional) – Limits the RAM during vocabulary building
-max_vocab_size = None
-
-# dm ({1,0}, optional) – Defines the training algorithm. If dm=1, ‘distributed memory’ (PV-DM) is used. Otherwise, distributed bag of words (PV-DBOW) is employed
-distributed_memory = 1
-
-# epochs (int, optional) – Number of iterations (epochs) over the corpus
-epochs = 100
+min_count = 5	# min_count (int, optional) – Ignores all words with total frequency lower than this
+max_vocab_size = None	# max_vocab_size (int, optional) – Limits the RAM during vocabulary building
+distributed_memory = 1	# dm ({1,0}, optional) – Defines the training algorithm. If dm=1, ‘distributed memory’ (PV-DM). Otherwise, distributed bag of words (PV-DBOW)
+epochs = 100	# epochs (int, optional) – Number of iterations (epochs) over the corpus
 
 
-
-# A function to build a model based on Doc2Vec, trained by our training documents
+# A function to build a model based on Doc2Vec, trained by our own training .t documents
 def buildDoc2VecModel(model_name, vector_size, window, alpha, min_alpha, min_count, distributed_memory, epochs):
 
-	# Get all text files in the training documents folder
-	training_documents = glob.glob(_TRAINING_T_FOLDER+"*.t")
+	training_files = glob.glob(_TRAINING_T_FOLDER+"*.t")	# Get all .t files in the training documents folder
 
-	# A corpus with all the training documents
-	training_corpus = []
+	training_lists = []	# each member is a list of words representing a file contents 
 
-	# Add the training_documents to the training_corpus
-	for training_document in training_documents:
-		list_list_words = pickle.load(open(training_document, "rb" ))  # list_list_words is a list of lists (each list are teh words of a sentence) 
+	# Add the content of the training_files to the training_corpus
+	for training_file in training_files:
+		list_list_words = pickle.load(open(training_file, "rb" ))  # list_list_words is a list of lists (each list are the words of a sentence) 
 		list_words  = [item for sublist in list_list_words for item in sublist] # flat list with all words of the document
-		training_corpus.append(list_words)
+		training_lists.append(list_words)
 
-	# Tag the training documents
-	tagged_training_corpus = [TaggedDocument(words=l, tags=[str(i)]) for i, l in enumerate(training_corpus)]
+	# Tag the training lists (add an increasing number as tag)
+	tagged_training_lists = [TaggedDocument(words=l, tags=[str(i)]) for i, l in enumerate(training_lists)]
 
-	# Create a Doc2Vec model
+	# this is the input for training
+	# tagged_training_lists is a list [TaggedDocument(['token1','token2',...], ['0']), TaggedDocument(['token1','token2',...], ['1']), ...]
+	
+	# Create a Doc2Vec model with the selected parameters
 	model = Doc2Vec(vector_size=vector_size,
 					window=window,
 					alpha=alpha,
@@ -79,21 +59,20 @@ def buildDoc2VecModel(model_name, vector_size, window, alpha, min_alpha, min_cou
 					min_count=min_count,
 					dm=distributed_memory)
 
-	# Build vocabulary from the tagged training corpus
-	model.build_vocab(tagged_training_corpus)
+	# Build vocabulary from the tagged training lists
+	model.build_vocab(tagged_training_lists)
 
-	# Train the model
-	model.train(tagged_training_corpus,
+	# Train the model from the tagged training lists
+	model.train(tagged_training_lists,
 				total_examples=model.corpus_count,
 				epochs=epochs,
 				start_alpha=alpha,
 				end_alpha=min_alpha)
 
-
 	# Save the trained model to file
 	model.save(model_name)
-	print("Doc2Vec model saved!")
+	print(model_name, "model saved!")
 
 
 # Build a doc2vec model trained with files in textos originales folder
-buildDoc2VecModel(model_full_filename, vector_size, window, alpha, min_alpha, min_count, distributed_memory, epochs)
+buildDoc2VecModel(model_filename, vector_size, window, alpha, min_alpha, min_count, distributed_memory, epochs)
--- module_train/buildD2VOwnModel-txt.py
+++ module_train/buildD2VOwnModel-txt.py
@@ -1,74 +1,59 @@
-# with 100 epochs and .txt
+# train with 100 epochs and .txt files
 
 import glob
 import os
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 
-from aux import CORPUS_FOLDER as _CORPUS_FOLDER
-if not os.path.exists(_CORPUS_FOLDER):
-	print(_CORPUS_FOLDER, "not found!")
-	exit()
+from aux import TRAINING_TXT_FOLDER as _TRAINING_TXT_FOLDER, OWN_D2V_MODEL as _OWN_D2V_MODEL
 	
-from aux import TRAINING_TXT_FOLDER as _TRAINING_TXT_FOLDER
 if not os.path.exists(_TRAINING_TXT_FOLDER):
 	print(_TRAINING_TXT_FOLDER, "not found!")
 	exit()
 
+# variables init
 
-from aux import OWN_D2V_MODEL as _OWN_D2V_MODEL
-model_full_filename = _OWN_D2V_MODEL + "-txt.model"
+model_filename = _OWN_D2V_MODEL + "-txt.model"
 
-
-# vector_size (int, optional) – Dimensionality of the feature vectors
-vector_size = 20
-
-# window (int, optional) – The maximum distance between the current and predicted word within a sentence
-window = 8
-
-# alpha (float, optional) – The initial learning rate
-alpha = 0.025
-
-# min_alpha (float, optional) – Learning rate will linearly drop to min_alpha as training progresses
-min_alpha = 0.00025
+vector_size = 20	# vector_size (int, optional) – Dimensionality of the feature vectors
+window = 8	# window (int, optional) – The maximum distance between the current and predicted word within a sentence
+alpha = 0.025	# alpha (float, optional) – The initial learning rate
+min_alpha = 0.00025	# min_alpha (float, optional) – Learning rate will linearly drop to min_alpha as training progresses
 
 # seed (int, optional) – Seed for the random number generator. Initial vectors for each word are seeded with a hash of the concatenation of word + str(seed)
 # seed = 1
 
-# min_count (int, optional) – Ignores all words with total frequency lower than this
-min_count = 1
-
-# max_vocab_size (int, optional) – Limits the RAM during vocabulary building
-max_vocab_size = None
-
-# dm ({1,0}, optional) – Defines the training algorithm. If dm=1, ‘distributed memory’ (PV-DM) is used. Otherwise, distributed bag of words (PV-DBOW) is employed
-distributed_memory = 1
-
-# epochs (int, optional) – Number of iterations (epochs) over the corpus
-epochs = 100
-
-# delta - To decrease the learning rate manually
-alpha_delta = 0.0002
+min_count = 1	# min_count (int, optional) – Ignores all words with total frequency lower than this
+max_vocab_size = None	# max_vocab_size (int, optional) – Limits the RAM during vocabulary building
+distributed_memory = 1	# dm ({1,0}, optional) – Defines the training algorithm. If dm=1, ‘distributed memory’ (PV-DM). Otherwise, distributed bag of words (PV-DBOW)
+epochs = 100	# epochs (int, optional) – Number of iterations (epochs) over the corpus
+alpha_delta = 0.0002	# delta - To decrease the learning rate manually
 
 
-# A function to build a model based on Doc2Vec, trained by our training documents
+
+
+# A function to build a model based on Doc2Vec, trained by our own training text documents
 def buildDoc2VecModel(model_name, vector_size, window, alpha, min_alpha, min_count, distributed_memory, epochs, alpha_delta):
 
-	# Get all text files in the training documents folder
-	training_documents = glob.glob(_TRAINING_TXT_FOLDER+"*.txt")
+	training_files = glob.glob(_TRAINING_TXT_FOLDER+"*.txt")	# Get all .txt files in the training documents folder
 
-	# A corpus with all the training documents
-	training_corpus = []
+	training_texts = []	# A corpus with a list of strings: each string is the text content of one training file
 
-	# Add the training_documents to the training_corpus
-	for training_document in training_documents:
-		training_text = open(training_document, "r")
-		training_corpus.append(training_text.read())
+	# read the training_files and add their contents to the training_texts list
+	for training_file in training_files:
+		training_fd = open(training_file, "r")
+		text = training_fd.read()
+		training_texts.append(text)
 
-	# Tag the training documents
-	tagged_training_corpus = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(training_corpus)]
+	# Tokenize and tag (add an increasing number as tag) the training texts
+	# word_tokenize(_text.lower()) changes text to lower and tokenizes it: creates a list with the different words of the text
+	tagged_training_lists = [TaggedDocument(words=word_tokenize(_text.lower()), tags=[str(i)]) for i, _text in enumerate(training_texts)]
 
-	# Create a Doc2Vec model
+	# this is the input for training
+	# tagged_training_lists is a list [TaggedDocument(['token1','token2',...], ['0']), TaggedDocument(['token1','token2',...], ['1']), ...]
+	
+	
+	# Create an empty Doc2Vec model with the selected parameters
 	model = Doc2Vec(vector_size=vector_size,
 					window=window,
 					alpha=alpha,
@@ -76,21 +61,21 @@ def buildDoc2VecModel(model_name, vector_size, window, alpha, min_alpha, min_cou
 					min_count=min_count,
 					dm=distributed_memory)
 
-	# Build vocabulary from the tagged training corpus
-	model.build_vocab(tagged_training_corpus)
+	# Build model vocabulary from the tagged training lists
+	model.build_vocab(tagged_training_lists)
 
-	# Train the model
-	model.train(tagged_training_corpus,
+	# Train the model with the tagged list of lists
+	model.train(tagged_training_lists,
 				total_examples=model.corpus_count,
 				epochs=epochs,
 				start_alpha=alpha,
 				end_alpha=min_alpha)
 
-
 	# Save the trained model to file
 	model.save(model_name)
-	print("Doc2Vec model saved!")
+	print(model_name, "model saved!")
 
+# start execution
 
 # Build a doc2vec model trained with files in textos originales folder
-buildDoc2VecModel(model_full_filename, vector_size, window, alpha, min_alpha, min_count, distributed_memory, epochs, alpha_delta)
+buildDoc2VecModel(model_filename, vector_size, window, alpha, min_alpha, min_count, distributed_memory, epochs, alpha_delta)
